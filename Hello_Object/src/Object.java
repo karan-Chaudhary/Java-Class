e6d69bac9618f8ebd25779a4520a733709835f51
Karan Zais <Karan Zais>
2016-03-18 10:35:16 +0545

    Mero Change

---
+++ Hello_Object/src/Object.java
@@ -0,0 +1,22 @@
+
+public class Object {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	
+	Nepal visa=new Nepal();
+	visa.getDisplay();
+	visa.getEducation();
+	
+			
+		}
+	}
+class Nepal{
+ public void getDisplay(){
+	 System.out.println("I am in USA");
+ }
+	 
+	 public void getEducation(){
+		 System.out.println("I had Successfully passed M.Ad");
+	 }
+}
